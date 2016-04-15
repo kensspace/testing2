8d173027005f42041e3e13bca0a2c3b66f0921a9
Tang, Yukun <yukun.tang@sap.com>
2016-04-16 00:01:47 +0800

    add config file

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from config import config
 
 
+# Factory to create a Flask app
 def create_app(config_name):
 
     app = Flask(__name__)
--- tests/test_create_app.py
+++ tests/test_create_app.py
@@ -1,7 +1,8 @@
-from unittest import TestCase
+import unittest
+from app import create_app
 
 
-class TestCreate_app(TestCase):
+class TestCreate_app(unittest.TestCase):
     # Initial UT
     def setUp(self):
         pass
@@ -11,4 +12,17 @@ class TestCreate_app(TestCase):
         pass
 
     def test_create_app(self):
-        pass
+        self.test_app = create_app("testing")
+        self.test_app_context = self.test_app.app_context()
+        self.test_app_context.push()
+        self.test_client = self.test_app.test_client()
+        self.test_app_context.pop()
+
+        self.dev_app = create_app("development")
+        self.dev_app_context = self.dev_app.app_context()
+        self.dev_app_context.push()
+        self.dev_client = self.dev_app.test_client()
+        self.dev_app_context.pop()
+
+if __name__ == '__main__':
+    unittest.main()
